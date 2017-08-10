4365ae28636b926b3d500f1839fb400f42402cc3
Dean Levinson <dean.e.levinson@gmail.com>
2017-08-10 11:04:29 +1000

    Adjust indentation

--- admin/assets/page_content.php
+++ admin/assets/page_content.php
@@ -12,7 +12,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <?php if ($title == "colour") { ?>
-    <script src="../assets/main.js"></script>
+<script src="../assets/main.js"></script>
   <?php } ?>
 </head>
 <body>
