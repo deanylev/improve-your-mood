a868a1d94ae6de0dec7069717bcfd4a0b6d49a73
Dean Levinson <dean.e.levinson@gmail.com>
2017-08-13 14:51:02 +1000

    Update MySQL credentials

--- sql.php
+++ sql.php
@@ -1,10 +1,11 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
 if ($_SERVER["HTTP_HOST"] == "localhost") {
+    $username = "root";
     $password = "";
 } else {
-    $password = "De6KAY9YZ8TJutWt";
+    $username = "admin";
+    $password = "c50514c96d3094955f934a89306ebd2ec8098251829c17d3";
 }
 $conn = mysqli_connect($servername, $username, $password);
