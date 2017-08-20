d815f8e00bc0c770d99180f021426e1ec0c7ee12
Dean Levinson <dean.e.levinson@gmail.com>
2017-08-20 19:29:10 +1000

    Clean up SQL credentials file

--- sql.php
+++ sql.php
@@ -1,11 +1,6 @@
 <?php
 
 $servername = "localhost";
-if ($_SERVER["HTTP_HOST"] == "localhost") {
-    $username = "root";
-    $password = "";
-} else {
-    $username = "admin";
-    $password = "c50514c96d3094955f934a89306ebd2ec8098251829c17d3";
-}
+$username = $_SERVER["HTTP_HOST"] == "localhost" ? "root" : "admin";
+$password = $_SERVER["HTTP_HOST"] == "localhost" ? "" : "c50514c96d3094955f934a89306ebd2ec8098251829c17d3";
 $conn = mysqli_connect($servername, $username, $password);
