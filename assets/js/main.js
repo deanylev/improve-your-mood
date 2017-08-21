18176642779c4ff6e0345265650caa332c4115b8
Dean Levinson <dean.e.levinson@gmail.com>
2017-08-21 10:13:15 +1000

    Add comments to JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,15 @@ var appError;
 
 $(document).ready(function() {
 
+  // Initialize Materialize Plugins
+
   $('.modal').modal();
 
   $('.tooltipped').tooltip({
     html: true
   });
+
+  // Put in correct text / year
 
   $('title').text(`${version} Your Mood`);
   $('#logo-version').text(version.toLowerCase());
@@ -24,13 +28,9 @@ $(document).ready(function() {
 
   $('#year').text(new Date().getFullYear());
 
-  $('#retry-button').click(function() {
-
-    window.location.reload();
-
-  });
-
 });
+
+// Function for displaying and logging errors
 
 function engineError(display, log, code) {
 
@@ -42,9 +42,13 @@ function engineError(display, log, code) {
 
 }
 
+// Pull quotes from backend
+
 console.log('Pulling quotes from backend...');
 
 $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.php`)
+
+  // Move on to next step
 
   .done(function(data) {
 
@@ -56,9 +60,13 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
     console.log(`Successfully pulled ${quotes.length} quotes from backend.`);
 
+    // Pull colours from backend
+
     console.log('Pulling colours from backend...');
 
     $.getJSON('http://improveyourmood.xyz/colour_serializer.php')
+
+      // Move on to next step
 
       .done(function(data) {
 
@@ -77,9 +85,13 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
         var settings;
         var backPressed;
 
+        // Pull settings from backend
+
         console.log('Pulling settings from backend...');
 
         $.getJSON('http://improveyourmood.xyz/settings_serializer.php')
+
+          // Move on to next step
 
           .done(function(data) {
 
@@ -93,7 +105,11 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
             console.log(`Successfully pulled ${Object.keys(settings).length} settings from backend.`);
 
+            // Function for reloading the quote
+
             reloadQuote = function() {
+
+              // If backend has no quotes, throw an error
 
               if (quotes.length === 0) {
 
@@ -105,11 +121,15 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
               lastNum = quoteNum;
               quoteNum = Math.floor(quotes.length * Math.random());
 
+              // If all quotes are used and no repeats is enabled, start again
+
               if (usedQuotes.length === quotes.length && settings['no_repeats']) {
 
                 usedQuotes = [];
 
               }
+
+              // If MoodEngine trys to use the same quote twice, or one that has already been used, generate a new one
 
               while (lastNum === quoteNum || settings['no_repeats'] && usedQuotes.includes(quoteNum)) {
 
@@ -117,16 +137,24 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
               }
 
+              // Add quote to used quotes and quote history
+
               usedQuotes.push(quoteNum);
               quoteHistory.push(quoteNum);
 
               let quote = quotes[quoteNum];
 
+              // Display quote on the text element
+
               $('#quote').text(quote);
 
             };
 
+            // Function for reloading the colour
+
             reloadColour = function() {
+
+              // If backend has no colours, throw an error
 
               if (colours.length === 0) {
 
@@ -138,20 +166,28 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
               lastNum = colourNum;
               colourNum = Math.floor(colours.length * Math.random());
 
+              // If MoodEngine trys to use the same colour twice, generate a new one
+
               while (lastNum === colourNum) {
 
                 colourNum = Math.floor(colours.length * Math.random());
 
               }
 
+              // Add colour to used quotes and quote history
+
               usedColours.push(colourNum);
               colourHistory.push(colourNum);
 
               let colour = colours[colourNum];
 
+              // Apply colour to background
+
               $('body').css('background-color', `#${colour}`);
 
             };
+
+            // Auto reload
 
             window.setInterval(function() {
 
@@ -163,6 +199,8 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
             }, localStorage.getItem('reload_interval') ? localStorage.getItem('reload_interval') : settings['reload_interval']);
 
+            // Set the correct values in the settings inputs
+
             $('.settings-input').each(function() {
 
               let setting = $(this).attr('name');
@@ -172,6 +210,8 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
             });
 
+            // Toggle auto reload when the button is clicked
+
             $('#toggle-auto-reload').click(function() {
 
               $('#auto-reload-icon').text($('#auto-reload-icon').text() === 'autorenew' ? 'close' : 'autorenew');
@@ -179,6 +219,8 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
               Materialize.toast('Auto Reload Toggled!', settings['toast_interval']);
 
             });
+
+            // Try to initialize the MoodEngine if there are no errors
 
             if (!appError) {
 
@@ -190,6 +232,8 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
                 reloadColour();
                 $('.fixed-action-btn').removeClass('hide');
                 console.log('MoodEngine initialized.');
+
+                // If an error, display/log it
 
               } catch (error) {
 
@@ -204,11 +248,15 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
               let shortcuts = localStorage.getItem('reload_keys') ? localStorage.getItem('reload_keys') : settings['reload_keys'];
 
+              // If a key in the reload keys array is pressed, reload
+
               if (shortcuts.includes(e.which) && $('main').hasClass('manual-reload')) {
 
                 reloadEngine();
 
               }
+
+              // If a key in the back keys array is pressed, go back by generating quotes from the history arrays
 
               if (settings['back_key'].includes(e.which) && $('main').hasClass('manual-reload') && usedQuotes.length > 1) {
 
@@ -237,11 +285,15 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
           })
 
+          // If pulling settings from backend fails, display/log the error
+
           .fail(function(data) {
 
             engineError('Failed to contact server. Try again later.', 'Failed to pull settings from backend.', '1c');
 
           });
+
+        // Function to reload both quotes and colour
 
         function reloadEngine() {
 
@@ -249,6 +301,8 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
             reloadQuote();
             reloadColour();
+
+            // Set backpressed to false for going back
             backPressed = false;
 
           }
@@ -256,6 +310,8 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
         }
 
         $('main').click(function() {
+
+          // Reload when main element is clicked (which is most of DOM)
 
           if ($(this).hasClass('manual-reload')) {
 
@@ -265,6 +321,8 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
         });
 
+        // When user trys to save settings
+
         $('#save-settings-button').click(function() {
 
           let local_settings = {};
@@ -272,10 +330,15 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
           $('.settings-input').each(function() {
 
+            // Construct the object using values
             local_settings[$(this).attr('name')] = $(this).val();
+
+            // Detect if input is blank
             has_input = $(this).val() ? true : false;
 
           });
+
+          // If all inputs are not blank
 
           if (has_input) {
 
@@ -283,9 +346,13 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
               $.each(local_settings, function(key, val) {
 
+                // If the set value is the same as the default, just remove it from localStorage and use backend value
+
                 if (val === settings[key]) {
 
                   localStorage.removeItem(key);
+
+                  // Otherwise set it in localStorage
 
                 } else {
 
@@ -295,9 +362,12 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
               });
 
+              // Reload the page for settings to come into effect
+
               Materialize.toast('Settings Saved!', settings['toast_interval']);
               window.location.reload();
 
+              // Catch any unexpected errors and display/log them
 
             } catch (error) {
 
@@ -306,11 +376,15 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
             }
 
+            // Close the modal no matter what
+
             $('#settings-modal').modal('close');
 
           }
 
         });
+
+        // Set desired settings to default using the attr on the default button
 
         $('.default-button').each(function() {
 
@@ -323,11 +397,15 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
         });
 
+        // The form is just for show, don't allow submitting
+
         $('#settings-form').submit(function() {
 
           return false;
 
         });
+
+        // Functions to check what was pulled from the backend in the console
 
         whatQuotes = function() {
 
@@ -349,6 +427,8 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
 
       })
 
+      // If pulling colours from backend fails, display/log the error
+
       .fail(function(data) {
 
         engineError('Failed to contact server. Try again later.', 'Failed to pull colours from backend.', '1b');
@@ -356,6 +436,8 @@ $.getJSON(`http://improveyourmood.xyz/${version.toLowerCase()}_quote_serializer.
       });
 
   })
+
+  // If pulling quotes from backend fails, display/log the error
 
   .fail(function(data) {
 
