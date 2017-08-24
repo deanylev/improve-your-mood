7701f5e5b917a01329cce9989b03d6e1623ca2d6
Dean Levinson <dean.e.levinson@gmail.com>
2017-08-24 20:58:22 +1000

    Read SQL credentials from ini file

--- sql.php
+++ sql.php
@@ -1,6 +1,7 @@
 <?php
 
+$settings = parse_ini_file("settings.ini", true);
 $servername = "localhost";
-$username = $_SERVER["HTTP_HOST"] == "localhost" ? "root" : "admin";
-$password = $_SERVER["HTTP_HOST"] == "localhost" ? "" : "c50514c96d3094955f934a89306ebd2ec8098251829c17d3";
+$username = $settings["MYSQL"]["username"];
+$password = $settings["MYSQL"]["password"];
 $conn = mysqli_connect($servername, $username, $password);
