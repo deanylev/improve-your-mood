22006fea7675769a59cd8a735547f1ea01b4c220
Dean Levinson <dean.e.levinson@gmail.com>
2017-09-06 21:38:55 +1000

    Modify admin panel code

--- admin/settings/new.php
+++ admin/settings/new.php
@@ -10,14 +10,17 @@
   $label = $_POST["label"];
   $description = $_POST["description"];
   include("../../sql.php");
+
   try {
     $sql = "INSERT INTO yourmood.settings (active, setting, value, user, advanced, mobile, input, label, description) VALUES (1, '$content', '$value', '$user', '$advanced', '$mobile', '$input', '$label', '$description')";
     $_SESSION["alert"] = "Successfully created setting.";
   } catch (Exception $e) {
     $_SESSION["alert"] = "Failed to create setting. Error: ${e}";
   }
+
   if ($conn->query($sql) === false) {
       echo "Error: " . $sql . "<br>" . $conn->error;
   }
+  
   $conn->close();
   header("location: .");
