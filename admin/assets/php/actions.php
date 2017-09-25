1065aec79916dcfb787946ff1a94e1ee5c00df6c
Dean Levinson <dean.e.levinson@gmail.com>
2017-09-25 21:29:59 +1000

    Remove view action for colours/quotes

--- admin/assets/php/actions.php
+++ admin/assets/php/actions.php
@@ -5,6 +5,14 @@ switch ($title) {
     $actions = array("view", "delete");
     break;
 
+  case "quote":
+    $actions = array("create", "edit", "delete");
+    break;
+
+  case "colour":
+    $actions = array("create", "edit", "delete");
+    break;
+
   default:
     $actions = array("create", "view", "edit", "delete");
     break;
