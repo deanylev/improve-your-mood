98d5f40011942d9e6fc935e9a937f6c5f9fd69a3
Dean Levinson <dean.e.levinson@gmail.com>
2017-09-25 20:38:55 +1000

    Confirm form submits

--- admin/assets/php/footer.php
+++ admin/assets/php/footer.php
@@ -4,6 +4,13 @@
       <p class="text-muted">MoodBackend v2.0 | &copy; 2017 <a href="https://deanlevinson.com.au">Dean Levinson</a></p>
     </div>
   </footer>
+  <script>
+    $('form').submit(function() {
+
+      return confirm('Are you sure?');
+
+    });
+  </script>
 </body>
 
 </html>
