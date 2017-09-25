a423887685e875bf681dba80eaaff4c6153f5852
Dean Levinson <dean.e.levinson@gmail.com>
2017-09-25 19:00:41 +1000

    Add datetime to logs

--- api/create/log/index.php
+++ api/create/log/index.php
@@ -1,11 +1,13 @@
 <?php
 
+  date_default_timezone_set('Australia/Melbourne');
   header("Access-Control-Allow-Origin: *");
+  $datetime = date('d/m/Y h:i:s a', time());
   $ip = isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER['REMOTE_ADDR'];
   $version = $_POST["version"];
   $platform = $_POST["platform"];
   $log = $_POST["log"];
-  $sql = "INSERT INTO yourmood.logs (ip_address, version, platform, log) VALUES ('$ip', '$version', '$platform', '$log')";
+  $sql = "INSERT INTO yourmood.logs (sent_at, ip_address, version, platform, log) VALUES ('$datetime', '$ip', '$version', '$platform', '$log')";
   include("../../../sql.php");
   if ($conn->query($sql) === false) {
       echo "Error: " . $sql . "<br>" . $conn->error;
