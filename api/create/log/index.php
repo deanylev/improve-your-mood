d2a736a930bcea986f8da47c2ee60ba546c074dc
Dean Levinson <dean.e.levinson@gmail.com>
2017-09-29 10:16:41 +1000

    Use mysqli_real_escape_string on API POSTs

--- api/create/log/index.php
+++ api/create/log/index.php
@@ -4,9 +4,9 @@
   header("Access-Control-Allow-Origin: *");
   $datetime = date('d/m/Y h:i:s a', time());
   $ip = isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER['REMOTE_ADDR'];
-  $version = $_POST["version"];
-  $platform = $_POST["platform"];
-  $log = $_POST["log"];
+  $version = mysqli_real_escape_string ($_POST["version"]);
+  $platform = mysqli_real_escape_string ($_POST["platform"]);
+  $log = mysqli_real_escape_string($_POST["log"]);
   $sql = "INSERT INTO yourmood.logs (sent_at, ip_address, version, platform, log) VALUES ('$datetime', '$ip', '$version', '$platform', '$log')";
   include("../../../sql.php");
   if ($conn->query($sql) === false) {
