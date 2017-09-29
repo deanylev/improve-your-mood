a39b7ead4840d0cef22a26faa36bda091b9a79d9
Dean Levinson <dean.e.levinson@gmail.com>
2017-09-29 10:20:21 +1000

    Fix creating logs

--- api/create/log/index.php
+++ api/create/log/index.php
@@ -2,13 +2,13 @@
 
   date_default_timezone_set('Australia/Melbourne');
   header("Access-Control-Allow-Origin: *");
+  include("../../../sql.php");
   $datetime = date('d/m/Y h:i:s a', time());
   $ip = isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER['REMOTE_ADDR'];
-  $version = mysqli_real_escape_string ($_POST["version"]);
-  $platform = mysqli_real_escape_string ($_POST["platform"]);
-  $log = mysqli_real_escape_string($_POST["log"]);
+  $version = mysqli_real_escape_string($conn, $_POST["version"]);
+  $platform = mysqli_real_escape_string($conn, $_POST["platform"]);
+  $log = mysqli_real_escape_string($conn, $_POST["log"]);
   $sql = "INSERT INTO yourmood.logs (sent_at, ip_address, version, platform, log) VALUES ('$datetime', '$ip', '$version', '$platform', '$log')";
-  include("../../../sql.php");
   if ($conn->query($sql) === false) {
       echo "Error: " . $sql . "<br>" . $conn->error;
   }
