1e84dea1e2876fb4a2a3c3a51679c2fac57190e9
Dean Levinson <dean.e.levinson@gmail.com>
2017-10-04 22:05:10 +1100

    Error if no quote version specified in API

--- api/get/quotes/index.php
+++ api/get/quotes/index.php
@@ -1,5 +1,9 @@
 <?php
 
-  $table = $_GET["version"];
-  $field = "quote";
-  include("../../../assets/php/array_serializer.php");
+  if (isset($_GET["version"])) {
+    $table = $_GET["version"];
+    $field = "quote";
+    include("../../../assets/php/array_serializer.php");
+  } else {
+    echo "Quote version not specified.";
+  }
