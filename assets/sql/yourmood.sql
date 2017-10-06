5fa0b1346ffb4adf9328af1e6dd3aec9ef021d34
Dean Levinson <dean.e.levinson@gmail.com>
2017-10-06 19:46:30 +1100

    Update SQL import script

--- assets/sql/yourmood.sql
+++ assets/sql/yourmood.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 04, 2017 at 07:59 AM
+-- Generation Time: Oct 06, 2017 at 08:46 AM
 -- Server version: 5.7.19-0ubuntu0.16.04.1
 -- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
@@ -226,6 +226,7 @@ CREATE TABLE `settings` (
   `advanced` tinyint(1) NOT NULL,
   `mobile` tinyint(1) NOT NULL,
   `input` varchar(10) NOT NULL,
+  `options` text NOT NULL,
   `tab` varchar(15) NOT NULL,
   `min` int(6) NOT NULL,
   `max` int(6) NOT NULL,
@@ -237,36 +238,37 @@ CREATE TABLE `settings` (
 -- Dumping data for table `settings`
 --
 
-INSERT INTO `settings` (`id`, `active`, `setting`, `value`, `user`, `optional`, `advanced`, `mobile`, `input`, `tab`, `min`, `max`, `label`, `description`) VALUES
-(1, 1, 'reload_interval', '3000', 1, 0, 0, 1, 'range', 'behaviour', 0, 10000, 'Auto Reload Interval (ms)', 'How often the auto reload will function, in milliseconds.'),
-(4, 1, 'toast_interval', '2000', 0, 0, 0, 0, 'number', '', 0, 0, '', ''),
-(10, 1, 'reload_keys', '["space", "enter", "right", "d"]', 1, 1, 0, 0, 'chips', 'keyboard', 0, 0, 'Reload Keyboard Shortcuts', 'Which keys will activate a reload.'),
-(11, 1, 'no_repeats', 'true', 0, 0, 0, 0, '', '', 0, 0, '', ''),
-(15, 1, 'back_keys', '["backspace", "left", "a"]', 1, 1, 0, 0, 'chips', 'keyboard', 0, 0, 'Rewind Keyboard Shortcuts', 'Which keys will go back to the previous quote/colour.'),
-(17, 1, 'auto_reload_keys', '["r"]', 1, 1, 0, 0, 'chips', 'keyboard', 0, 0, 'Auto Reload Keyboard Shortcuts', 'Which keys will toggle auto reloading.'),
-(18, 1, 'settings_keys', '["s", "up", "down"]', 1, 1, 0, 0, 'chips', 'keyboard', 0, 0, 'Toggle Settings Panel Keyboard Shortcuts', 'Which keys will toggle the settings panel.'),
-(19, 1, 'save_settings_keys', '["enter"]', 0, 0, 0, 0, '', '', 0, 0, '', ''),
-(20, 1, 'backend_address', 'improveyourmood.xyz', 1, 0, 1, 1, 'text', 'advanced', 0, 0, 'Custom Back-End Address', 'Define a custom back-end address, rather than using the live one. Used for development purposes.'),
-(22, 1, 'colour_reload_transitions', 'true', 1, 0, 0, 1, 'select', 'appearance', 0, 0, 'Colour Reload Transitions', 'Whether the background colour will have a fade transition when changing.'),
-(23, 1, 'extra_logging', '[]', 0, 0, 0, 0, '', '', 0, 0, '', ''),
-(24, 1, 'colour_reload_transition_time', '1200', 1, 0, 0, 1, 'range', 'appearance', 100, 5000, 'Colour Reload Transition Time (ms)', 'How long the colour reload transitions will take to complete, in milliseconds.'),
-(25, 1, 'text_reload_transitions', 'true', 1, 0, 0, 1, 'select', 'appearance', 0, 0, 'Text Reload Transitions', 'Whether the text will have a fade transition when changing.'),
-(26, 1, 'text_reload_transition_time', '400', 1, 0, 0, 1, 'range', 'appearance', 100, 5000, 'Text Reload Transition Time (ms)', 'How long the text reload transitions will take to complete, in milliseconds.'),
-(29, 1, 'app_version', '6.7.0', 0, 0, 0, 0, '', '', 0, 0, '', ''),
-(30, 1, 'app_update_reminder', 'true', 0, 0, 0, 0, '', '', 0, 0, '', 'Please update to the latest version of the app.'),
-(33, 1, 'reverse_swipe_direction', 'false', 1, 0, 0, 1, 'select', 'behaviour', 0, 0, 'Reverse Swipe Direction', 'Reverse the directions that you swipe to reload/rewind.'),
-(34, 1, 'button_icons', '{"menu":"menu", "autoreload":"autorenew", "settings":"settings", "rewind":"skip_previous", "switchversion":"swap_calls", "setalldefault":"clear_all", "fullrewind":"first_page"}', 0, 0, 0, 0, 'chips', 'keyboard', 0, 0, '', ''),
-(35, 1, 'require_settings_reload', 'false', 0, 0, 0, 0, 'select', '', 0, 0, '', ''),
-(36, 1, 'menu_keys', '["m"]', 1, 1, 0, 0, 'chips', 'keyboard', 0, 0, 'Menu Keyboard Shortcuts', 'Which keys will toggle the menu.'),
-(37, 1, 'scroll_settings', 'false', 0, 0, 0, 0, 'select', '', 0, 0, '', ''),
-(38, 1, 'button_order', '["autoreload", "settings", "rewind"]', 1, 0, 0, 1, 'chips', 'appearance', 0, 0, 'Button Menu Order', 'The order of the buttons in the button menu. Alternatively, these can be reordered by clicking and dragging them around. Any values that aren\'t apart of the default 3 will be ignored.'),
-(39, 1, 'theme_colour', 'white', 1, 0, 0, 1, 'text', 'appearance', 0, 0, 'Theme Colour', 'What colour the accents (text, icons) will appear. Needs to be a valid CSS colour (eg. white, black, red, orange, green, etc.).'),
-(40, 1, 'full_rewind_keys', '["shift+backspace"]', 1, 1, 0, 0, 'chips', 'keyboard', 0, 0, 'Full Rewind Keyboard Shortcuts', 'Which keys will go back to the first quote/colour.'),
-(41, 1, 'optional_indicators', 'true', 0, 0, 0, 0, 'select', '', 0, 0, '', ''),
-(44, 1, 'reset_input_buttons', 'false', 0, 0, 0, 0, '', '', 0, 0, '', ''),
-(45, 1, 'keep_advanced_settings', 'false', 1, 0, 1, 1, 'select', 'advanced', 0, 0, 'Keep Advanced Settings', 'If enabled, advanced settings will not be set to default when \'Set All to Default\' is clicked. This is useful when testing on a custom back-end address.'),
-(48, 1, 'tabs', '["appearance", "behaviour", "keyboard", "advanced"]', 0, 0, 0, 0, '', '', 0, 0, '', ''),
-(50, 1, 'keep_tab', 'true', 0, 0, 0, 0, '', '', 0, 0, '', '');
+INSERT INTO `settings` (`id`, `active`, `setting`, `value`, `user`, `optional`, `advanced`, `mobile`, `input`, `options`, `tab`, `min`, `max`, `label`, `description`) VALUES
+(1, 1, 'reload_interval', '3000', 1, 0, 0, 1, 'range', '', 'behaviour', 0, 10000, 'Auto Reload Interval (ms)', 'How often the auto reload will function, in milliseconds.'),
+(4, 1, 'toast_interval', '2000', 0, 0, 0, 0, 'number', '', '', 0, 0, '', ''),
+(10, 1, 'reload_keys', '["space", "enter", "right", "d"]', 1, 1, 0, 0, 'chips', '', 'keyboard', 0, 0, 'Reload Keyboard Shortcuts', 'Which keys will activate a reload.'),
+(11, 1, 'no_repeats', 'true', 0, 0, 0, 0, '', '', '', 0, 0, '', ''),
+(15, 1, 'back_keys', '["backspace", "left", "a"]', 1, 1, 0, 0, 'chips', '', 'keyboard', 0, 0, 'Rewind Keyboard Shortcuts', 'Which keys will go back to the previous quote/colour.'),
+(17, 1, 'auto_reload_keys', '["r"]', 1, 1, 0, 0, 'chips', '', 'keyboard', 0, 0, 'Auto Reload Keyboard Shortcuts', 'Which keys will toggle auto reloading.'),
+(18, 1, 'settings_keys', '["s", "up", "down"]', 1, 1, 0, 0, 'chips', '', 'keyboard', 0, 0, 'Toggle Settings Panel Keyboard Shortcuts', 'Which keys will toggle the settings panel.'),
+(19, 1, 'save_settings_keys', '["enter"]', 0, 0, 0, 0, '', '', '', 0, 0, '', ''),
+(20, 1, 'backend_address', 'improveyourmood.xyz', 1, 0, 1, 1, 'text', '', 'advanced', 0, 0, 'Custom Back-End Address', 'Define a custom back-end address, rather than using the live one. Used for development purposes.'),
+(22, 1, 'colour_reload_transitions', 'true', 1, 0, 0, 1, 'select', '', 'appearance', 0, 0, 'Colour Reload Transitions', 'Whether the background colour will have a fade transition when changing.'),
+(23, 1, 'extra_logging', '[]', 0, 0, 0, 0, '', '', '', 0, 0, '', ''),
+(24, 1, 'colour_reload_transition_time', '1200', 1, 0, 0, 1, 'range', '', 'appearance', 100, 5000, 'Colour Reload Transition Time (ms)', 'How long the colour reload transitions will take to complete, in milliseconds.'),
+(25, 1, 'text_reload_transitions', 'true', 1, 0, 0, 1, 'select', '', 'appearance', 0, 0, 'Text Reload Transitions', 'Whether the text will have a fade transition when changing.'),
+(26, 1, 'text_reload_transition_time', '400', 1, 0, 0, 1, 'range', '', 'appearance', 100, 5000, 'Text Reload Transition Time (ms)', 'How long the text reload transitions will take to complete, in milliseconds.'),
+(29, 1, 'app_version', '6.7.0', 0, 0, 0, 0, '', '', '', 0, 0, '', ''),
+(30, 1, 'app_update_reminder', 'true', 0, 0, 0, 0, '', '', '', 0, 0, '', 'Please update to the latest version of the app.'),
+(33, 1, 'reverse_swipe_direction', 'false', 1, 0, 0, 1, 'select', '', 'behaviour', 0, 0, 'Reverse Swipe Direction', 'Reverse the directions that you swipe to reload/rewind.'),
+(34, 1, 'button_icons', '{"menu":"menu", "autoreload":"autorenew", "settings":"settings", "rewind":"skip_previous", "switchversion":"swap_calls", "setalldefault":"clear_all", "fullrewind":"first_page"}', 0, 0, 0, 0, 'chips', '', 'keyboard', 0, 0, '', ''),
+(35, 1, 'require_settings_reload', 'false', 0, 0, 0, 0, 'select', '', '', 0, 0, '', ''),
+(36, 1, 'menu_keys', '["m"]', 1, 1, 0, 0, 'chips', '', 'keyboard', 0, 0, 'Menu Keyboard Shortcuts', 'Which keys will toggle the menu.'),
+(37, 1, 'scroll_settings', 'false', 0, 0, 0, 0, 'select', '', '', 0, 0, '', ''),
+(38, 1, 'button_order', '["autoreload", "settings", "rewind"]', 1, 0, 0, 1, 'chips', '', 'appearance', 0, 0, 'Button Menu Order', 'The order of the buttons in the button menu. Alternatively, these can be reordered by clicking and dragging them around. Any values that aren\'t apart of the default 3 will be ignored.'),
+(39, 1, 'theme_colour', 'white', 1, 0, 0, 1, 'select', '["aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgrey", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "grey", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgrey", "lightgreen", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"]', 'appearance', 0, 0, 'Theme Colour', 'What colour the accents (text, icons) will appear. Needs to be a valid CSS colour (eg. white, black, red, orange, green, etc.).'),
+(40, 1, 'full_rewind_keys', '["shift+backspace"]', 1, 1, 0, 0, 'chips', '', 'keyboard', 0, 0, 'Full Rewind Keyboard Shortcuts', 'Which keys will go back to the first quote/colour.'),
+(41, 1, 'optional_indicators', 'true', 0, 0, 0, 0, 'select', '', '', 0, 0, '', ''),
+(44, 1, 'reset_input_buttons', 'false', 0, 0, 0, 0, '', '', '', 0, 0, '', ''),
+(45, 1, 'keep_advanced_settings', 'false', 1, 0, 1, 1, 'checkbox', '', 'advanced', 0, 0, 'Keep Advanced Settings', 'If enabled, advanced settings will not be set to default when \'Set All to Default\' is clicked. This is useful when testing on a custom back-end address.'),
+(48, 1, 'tabs', '["appearance", "behaviour", "keyboard", "advanced"]', 0, 0, 0, 0, '', '', '', 0, 0, '', ''),
+(50, 1, 'keep_tab', 'true', 0, 0, 0, 0, '', '', '', 0, 0, '', ''),
+(51, 1, 'disable_caching', 'false', 1, 0, 1, 1, 'checkbox', '', 'advanced', 0, 0, 'Disable Caching', 'If enabled, quotes and colours will not be cached, and fresh copies will be pulled every time the page loads.');
 
 --
 -- Indexes for dumped tables
@@ -325,12 +327,12 @@ ALTER TABLE `improve`
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=407;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=436;
 --
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
