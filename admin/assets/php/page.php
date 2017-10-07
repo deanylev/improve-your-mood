f8f42258d43f73ebe8a59958bb240ce160930aa0
Dean Levinson <dean.e.levinson@gmail.com>
2017-10-07 13:00:12 +1100

    Make admin tables responsive

--- admin/assets/php/page.php
+++ admin/assets/php/page.php
@@ -20,57 +20,59 @@
   <br><br>
 </form>
 
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <?php if (!isset($notDefault)): ?>
-        <th><?php echo ucwords($title); ?></th>
-      <?php endif; ?>
-      <?php echo isset($customHeadings) ? $customHeadings : ""; ?>
-      <?php if (in_array("edit", $actions) || in_array("delete", $actions)): ?>
-        <th>Actions</th>
-      <?php endif; ?>
-    </tr>
-  </thead>
-  <tbody>
+<div class="table-responsive">
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <?php if (!isset($notDefault)): ?>
+          <th><?php echo ucwords($title); ?></th>
+        <?php endif; ?>
+        <?php echo isset($customHeadings) ? $customHeadings : ""; ?>
+        <?php if (in_array("edit", $actions) || in_array("delete", $actions)): ?>
+          <th>Actions</th>
+        <?php endif; ?>
+      </tr>
+    </thead>
+    <tbody>
 
-<?php
+  <?php
 
-  if ($result->num_rows > 0) {
-      while ($row = $result->fetch_assoc()):
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()):
 
-?>
+  ?>
 
-    <tr>
-      <?php if (!isset($notDefault)): ?>
-        <td><p><?php echo $row[$title]; ?></p></td>
-      <?php endif; ?>
-      <?php isset($customFields) ? include($customFields) : ""; ?>
-      <td>
-        <form action="../modify.php" method="POST">
-          <?php if (in_array("view", $actions)): ?>
-            <a class="btn btn-success" href="../view?type=<?php echo $table; ?>&amp;title=<?php echo $title; ?>&amp;id=<?php echo $row["id"]; ?>">View</a>
-          <?php endif; ?>
-          <?php if (in_array("edit", $actions)): ?>
-            <a class="btn btn-warning" href="../edit?type=<?php echo $table; ?>&amp;title=<?php echo $title; ?>&amp;id=<?php echo $row["id"]; ?>">Edit</a>
-          <?php endif; ?>
-          <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
-          <input type="hidden" name="table" value="<?php echo $table; ?>">
-          <input type="hidden" name="type" value="<?php echo $title; ?>">
-          <?php if (in_array("delete", $actions)): ?>
-            <input class="btn btn-danger" type="submit" name="delete" value="Delete">
-          <?php endif; ?>
-        </form>
-      </td>
-    </tr>
+      <tr>
+        <?php if (!isset($notDefault)): ?>
+          <td><p><?php echo $row[$title]; ?></p></td>
+        <?php endif; ?>
+        <?php isset($customFields) ? include($customFields) : ""; ?>
+        <td>
+          <form action="../modify.php" method="POST">
+            <?php if (in_array("view", $actions)): ?>
+              <a class="btn btn-success" href="../view?type=<?php echo $table; ?>&amp;title=<?php echo $title; ?>&amp;id=<?php echo $row["id"]; ?>">View</a>
+            <?php endif; ?>
+            <?php if (in_array("edit", $actions)): ?>
+              <a class="btn btn-warning" href="../edit?type=<?php echo $table; ?>&amp;title=<?php echo $title; ?>&amp;id=<?php echo $row["id"]; ?>">Edit</a>
+            <?php endif; ?>
+            <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
+            <input type="hidden" name="table" value="<?php echo $table; ?>">
+            <input type="hidden" name="type" value="<?php echo $title; ?>">
+            <?php if (in_array("delete", $actions)): ?>
+              <input class="btn btn-danger" type="submit" name="delete" value="Delete">
+            <?php endif; ?>
+          </form>
+        </td>
+      </tr>
 
-<?php
+  <?php
 
-    endwhile;
-  }
+      endwhile;
+    }
 
-?>
-  </tbody>
-</table>
+  ?>
+    </tbody>
+  </table>
+</div>
 
 <?php include("footer.php"); ?>
