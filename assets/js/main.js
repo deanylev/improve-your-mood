a44dba520dcf45c19702af0d6e1ad1800f4ea306
Dean Levinson <dean.e.levinson@gmail.com>
2017-10-08 16:53:30 +1100

    Clean up imported functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,20 +148,25 @@ $(document).ready(function() {
 
 function modifyColour(hex, lum) {
 
-  // validate hex string
-  hex = String(hex).replace(/[^0-9a-f]/gi, '');
-  if (hex.length < 6) {
+  hex = hex.replace(/[^0-9a-f]/gi, '');
+
+  if (hex.length === 3) {
+
     hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+
   }
+
   lum = lum || 0;
 
-  // convert to decimal and change luminosity
-  var rgb = '#',
-    c, i;
+  let c, i;
+  let rgb = '#';
+
   for (i = 0; i < 3; i++) {
+
     c = parseInt(hex.substr(i * 2, 2), 16);
     c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
-    rgb += ('00' + c).substr(c.length);
+    rgb += (`00${c}`).substr(c.length);
+
   }
 
   return rgb;
@@ -171,16 +176,25 @@ function modifyColour(hex, lum) {
 // Function for converting hex to rgba
 
 function hexToRgbA(hex, opacity) {
-  var c;
+
+  let c;
+
   if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+
     c = hex.substring(1).split('');
-    if (c.length == 3) {
+
+    if (c.length === 3) {
+
       c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+
     }
+
     c = '0x' + c.join('');
+
     return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',' + opacity + ')';
+
   }
-  throw new Error('Bad Hex');
+
 }
 
 // Function for changing colour
