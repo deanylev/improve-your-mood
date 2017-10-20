3da0cda21037416677a2fa42f72878a71814b5e6
Dean Levinson <dean.e.levinson@gmail.com>
2017-10-21 03:59:42 +1100

    Add read_only column to users

--- admin/users/index.php
+++ admin/users/index.php
@@ -2,4 +2,6 @@
 
   $table = "users";
   $type = $title = "user";
+  $customHeadings = "<th>Read Only</th>";
+  $customFields = "../assets/php/user_columns.php";
   include("../assets/php/page.php");
