03929e44d7c849ea0eb3b4ae6133751aff32f0f8
Dean Levinson <dean.e.levinson@gmail.com>
2017-10-23 09:20:13 +1100

    Add missing FA to reset page

--- admin/reset/index.php
+++ admin/reset/index.php
@@ -27,6 +27,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Admin Panel - Reset</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="../assets/css/main.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
