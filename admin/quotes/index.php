27e40321ba2719756390f43dec535d52a6cc4f1f
Dean Levinson <dean.e.levinson@gmail.com>
2017-10-29 13:44:09 +1100

    Don't allow no type param for quotes

--- admin/quotes/index.php
+++ admin/quotes/index.php
@@ -1,5 +1,12 @@
 <?php
 
-  $table = $type = $_GET["type"];
+  if (isset($_GET["type"])) {
+    $table = $type = $_GET["type"];
+  } else {
+    session_start();
+    $_SESSION["message"]["danger"] = "An error occured.";
+    header("location: ../home");
+    die();
+  }
   $title = "quote";
   include("../assets/php/page.php");
