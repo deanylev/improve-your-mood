3ad0ea5e9c68ea4a6491fe86924046a1b6911d2a
Dean Levinson <dean.e.levinson@gmail.com>
2017-11-05 23:17:07 +1100

    Add missing cookies file

---
+++ assets/php/cookies.php
@@ -0,0 +1,19 @@
+<?php
+
+  include("settings.php");
+
+  $key = $settings["KEYS"]["cookie"];
+
+  function encryptCookie($value)
+  {
+      global $key;
+      $newValue = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $value, MCRYPT_MODE_CBC, md5(md5($key))));
+      return($newValue);
+  }
+
+  function decryptCookie($value)
+  {
+      global $key;
+      $newValue = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($value), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
+      return($newValue);
+  }
