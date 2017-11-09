a54a8fdcede659ec4050b7c0e4813239dfe0356d
Dean Levinson <dean.e.levinson@gmail.com>
2017-11-09 15:17:46 +1100

    Add security so non-admins can only edit themself

--- admin/assets/php/actions.php
+++ admin/assets/php/actions.php
@@ -14,28 +14,32 @@
     // Delete All - delete all existing records in a table
 
     if (isset($type)) {
-      if (!$currentUser["is_admin"]) {
-        $actions = array("edit");
-      } elseif (!$currentUser["read_only"]) {
-        switch ($type) {
-          case "log":
-          case "logs":
-            $actions = array("view", "delete", "deleteall");
-            break;
+      if ($currentUser["is_admin"]) {
+        if (!$currentUser["read_only"]) {
+          switch ($type) {
+            case "log":
+            case "logs":
+              $actions = array("view", "delete", "deleteall");
+              break;
 
-          case "user":
-          case "users":
-            $actions = array("create", "edit", "delete");
-            break;
+            case "user":
+            case "users":
+              $actions = array("create", "edit", "delete");
+              break;
 
-          default:
-            $actions = array("create", "view", "edit", "delete");
-            break;
+            default:
+              $actions = array("create", "view", "edit", "delete");
+              break;
+          }
+        } elseif (($type === "user" || $type === "users") && isset($id) && $id == $_SESSION["user"]) {
+          $actions = array("edit");
+        } else {
+          $actions = array("view");
         }
       } elseif (($type === "user" || $type === "users") && isset($id) && $id == $_SESSION["user"]) {
-          $actions = array("edit");
+        $actions = array("edit");
       } else {
-          $actions = array("view");
+        $actions = array("");
       }
     }
   }
