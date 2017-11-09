0d7ede194eee62682d7fc639ed361f7e514ab37e
Dean Levinson <dean.e.levinson@gmail.com>
2017-11-09 22:02:47 +1100

    Hide app_settings from user edit page

--- admin/edit/index.php
+++ admin/edit/index.php
@@ -37,7 +37,7 @@
 
   set_error_handler("warning_handler", E_WARNING);
   foreach ($row as $key => $val) {
-      if ($key !== "id" && $key !== "password") {
+      if ($key !== "id" && $key !== "password" && $key !== "app_settings") {
       $columnType = $columnTypes[$key]; ?>
         <div class="default-field" data-key="<?php echo $key; ?>" data-value="<?php echo $val; ?>" <?php echo $columnType === 1 ? "data-bool=\"true\"" : "" ?>></div>
 <?php
