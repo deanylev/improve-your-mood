4e6cb266dde1e96dcccaa8504e82373f1f7e22b8
Dean Levinson <dean.e.levinson@gmail.com>
2017-12-06 23:52:19 +1100

    Fix up image display script

--- admin/users/image.php
+++ admin/users/image.php
@@ -4,10 +4,11 @@
   include("../../assets/php/sql.php");
   include("../assets/php/user.php");
 
-  $id = $_GET["id"];
-  $file = $mysqli->query("SELECT image FROM yourmood.users WHERE id = '{$id}'")->fetch_object()->image;
+  $id = @$_GET["id"];
+  $query = @$mysqli->query("SELECT * FROM yourmood.users WHERE id = '{$id}'");
+  $file = @$query->fetch_object()->image;
 
-  if ($file && file_exists("../uploads/images/user/" . $file)) {
+  if (isset($_GET["id"]) && $query->num_rows && $file && file_exists("../uploads/images/user/" . $file)) {
     $image = imagecreatefrompng("../uploads/images/user/" . $file);
   } else {
     $image = imagecreatefrompng("../uploads/images/user/placeholder.png");
