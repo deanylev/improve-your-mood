02a1bfc6c700d175c601582743cc5a268ca1f617
Dean Levinson <dean.e.levinson@gmail.com>
2017-12-27 09:26:55 +1100

    Remove accidental json_encode in export.php

--- admin/export.php
+++ admin/export.php
@@ -10,7 +10,7 @@
     $value["table"] = $table;
     $value = json_encode($value);
     $value = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $value, MCRYPT_MODE_CBC, md5(md5($key))));
-    echo json_encode($value);
+    echo $value;
   } else {
     die("unauthorised");
   }
