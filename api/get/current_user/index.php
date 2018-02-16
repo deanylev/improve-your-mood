28912da31ff4a9a3e2de56f83f142afe08a7bd26
Dean Levinson <dean.e.levinson@gmail.com>
2018-02-16 17:48:25 +1100

    Fix deleted user breaking app login

--- api/get/current_user/index.php
+++ api/get/current_user/index.php
@@ -11,16 +11,26 @@
     $_SESSION["user"] = decryptCookie($_COOKIE["user"]);
   }
 
-  if (isset($_SESSION["user"])) {
-    $sessionUser = $_SESSION["user"];
-    $userQuery = $mysqli->query("SELECT * FROM yourmood.users WHERE id='{$sessionUser}'");
-    $userRow = $userQuery->fetch_assoc();
-    $user["id"] = $userRow["id"];
-    $user["is_admin"] = $userRow["is_admin"] === "1" ? true : false;
-    $user["name"] = $userRow["user"];
-    $user["settings"] = htmlspecialchars_decode($userRow["app_settings"]);
-    header("Content-Type: application/json");
-    echo json_encode($user);
-  } else {
-    echo "no user";
+  function checkUser() {
+    global $mysqli;
+    if (isset($_SESSION["user"])) {
+      $userQuery = $mysqli->query("SELECT * FROM yourmood.users WHERE id='{$_SESSION["user"]}'");
+      if (!$userQuery->num_rows) {
+        unset($_SESSION["user"]);
+        setcookie("user", "", time() - 3600, "/");
+        checkUser();
+        die();
+      }
+      $userRow = $userQuery->fetch_assoc();
+      $user["id"] = $userRow["id"];
+      $user["is_admin"] = $userRow["is_admin"] === "1" ? true : false;
+      $user["name"] = $userRow["user"];
+      $user["settings"] = htmlspecialchars_decode($userRow["app_settings"]);
+      header("Content-Type: application/json");
+      echo json_encode($user);
+    } else {
+      echo "no user";
+    }
   }
+
+  checkUser();
